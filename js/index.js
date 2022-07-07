14b38e6d66951ede0165945537e8cd5215971726
MrRoiPanda <61798547+MrRoiPanda@users.noreply.github.com>
2022-07-07 21:49:33 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var menu_overlay = new Boolean("false")
+console.log(menu_overlay);
+document.getElementById("Nav-overlay").style = 'opacity: 0; display: none;';
+
+//detecter ke clique
+document.getElementById("spy-me").onmousedown = function () {
+  menu_overlay = !menu_overlay;
+
+  if (menu_overlay == false) { // on vois
+    document.getElementById("Nav-overlay").style = 'opacity: 1; display: block;';
+  }
+  if (menu_overlay == true) { // on vois pas
+    document.getElementById("Nav-overlay").style = 'opacity: 0; display: none;';
+  }
+};
